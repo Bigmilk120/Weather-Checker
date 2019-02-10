7575a0287ba415cc9ba9f0fe57843a380899fdd4
Bigmilk120 <sprawdzany96@gmail.com>
2019-02-10 19:06:17 +0100

    Weather Checker
    Trying to make background image.

--- src/DetailsFrame.java
+++ src/DetailsFrame.java
@@ -10,26 +10,31 @@ public class DetailsFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,400);
         setLocation(100,100);
+        JLabel jlb = new JLabel();
+        jlb.setIcon(new ImageIcon(getClass().getResource("rain.jpg")));
+        jlb.setSize(new Dimension(400,200));
 
         Container container = getContentPane();
         container.setLayout(new FlowLayout());
 
+
         JLabel temperature = new JLabel("Today's temperature is: "+DetailedInfo.getTemperature(data)+DEGREE+"C");
-        temperature.setPreferredSize(new Dimension(500,50));
+        temperature.setPreferredSize(new Dimension(500,25));
         JLabel minTemperature = new JLabel("The lowest temperature in the city: "+DetailedInfo.getMinTemperature(data)+DEGREE+"C");
-        minTemperature.setPreferredSize(new Dimension(500,50));
+        minTemperature.setPreferredSize(new Dimension(500,25));
         JLabel maxTemperature = new JLabel("The highest temperature in the city: "+DetailedInfo.getMaxTemperature(data)+DEGREE+"C");
-        maxTemperature.setPreferredSize(new Dimension(500,50));
+        maxTemperature.setPreferredSize(new Dimension(500,25));
         JLabel humidity = new JLabel("Humidity: "+DetailedInfo.getHumidity(data)+"%");
-        humidity.setPreferredSize(new Dimension(500,50));
+        humidity.setPreferredSize(new Dimension(500,25));
         JLabel pressure = new JLabel("Pressure: "+DetailedInfo.getPressure(data)+"hPa");
-        pressure.setPreferredSize(new Dimension(500,50));
+        pressure.setPreferredSize(new Dimension(500,25));
 
         container.add(temperature);
         container.add(minTemperature);
         container.add(maxTemperature);
         container.add(humidity);
         container.add(pressure);
+        container.add(jlb);
 
         setVisible(true);
     }
