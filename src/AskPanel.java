a0ba4f68b6346cd22fc6ac21ca949a612f8a3607
Bigmilk120 <sprawdzany96@gmail.com>
2019-02-13 17:52:46 +0100

    Weather Checker

--- src/AskPanel.java
+++ src/AskPanel.java
@@ -11,6 +11,10 @@ public class AskPanel extends JPanel {
 
         JTextField city = new JTextField();
         city.setPreferredSize(new Dimension(500,25));
+
+        JLabel wrong = new JLabel("Wrong city!");
+        wrong.setPreferredSize(new Dimension(500,25));
+
         ArrayList<String> words = cityList.getList();
         AutoSuggestor autoSuggestor = new AutoSuggestor(city, frame, null, Color.WHITE.brighter(), Color.BLUE, Color.RED, 0.75f) {
             @Override
@@ -25,11 +29,11 @@ public class AskPanel extends JPanel {
         add(city);
         add(confirm_city);
         confirm_city.addActionListener(e -> {
-            if(OutputGiver.getData(city.getText()).length()!=0){
+            try{
                 add(new BasicFrame(city));
                 city.setVisible(false);
                 confirm_city.setVisible(false);
-            }
+            }catch(Exception ex){}
         });
     }
 }
